e0dff71ca3c409d457aee77fbeb8356a19ddd568
Janusz Mordarski <janusz.m@gmail.com>
2017-01-24 15:13:29 +0100

    Update User policy, allow listing

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,5 +1,13 @@
 class UserPolicy < ApplicationPolicy
 
+  def index?
+    true
+  end
+
+  def show?
+    scope.where(id: record.id).exists?# && (@user.id == @record.id || @user.admin?)
+  end
+
   def permitted_attributes
     return [
       :first_name,
