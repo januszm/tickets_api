2f2e46538d669d7ad6ddbd6b9dd38f79493de4d2
Janusz Mordarski <janusz.m@gmail.com>
2017-01-26 23:25:40 +0100

    Serialize issue status

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,5 +1,5 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :status
   has_one :creator
   has_one :assignee
 end
